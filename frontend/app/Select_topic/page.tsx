62d280e76e4b2002a8009f915fce8e6aa178293b
unknown <yadhuvipin12@gmail.com>
2025-03-01 08:43:10 +0530

    update: edited physics landing page

--- frontend/app/Select_topic/page.tsx
+++ frontend/app/Select_topic/page.tsx
@@ -68,6 +68,7 @@ export default function SelectClass() {
           <h2 className="text-4xl font-bold mb-14 text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-purple-500">
             Select a Quiz for {subject}
           </h2>
+          <h3 className="text-4xl font-bold mb-14 text-transparent bg-clip-text bg-white"> Class 12</h3>
 
           <div className="grid grid-cols-2 gap-8 justify-center">
             {subject === "Physics" && physicsQuizzes.map((quiz, index) => (
